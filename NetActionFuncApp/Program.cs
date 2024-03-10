89646fb8fd15af61b3c189620db6d1e49c2e76a5
maxefimov <efimov.top.academy@gmail.com>
2024-03-10 10:51:44 +0300

    Добавьте файлы проекта.

---
+++ NetActionFuncApp/Program.cs
@@ -0,0 +1,40 @@
+﻿Action method = PrintHello;
+Action<string> method2 = PrintHelloName;
+
+Func<int> method100 = Return100;
+Func<float, int> squareFloatInt = SquareNumber;
+
+Predicate<float> predicate = Filter;
+
+bool Filter(float item)
+{
+    return item > 0;
+}
+
+
+int Return100()
+{
+    return 100;
+}
+
+int SquareNumber(float number)
+{
+    return (int)number * (int)number;
+}
+
+void PrintHello()
+{
+    Console.WriteLine("Hello");
+}
+
+void PrintHelloName(string name)
+{ 
+    Console.WriteLine($"Hello {name}"); 
+}
+
+
+// delegate void Method(); // Action
+
+delegate void Method1<T>( T var1 );
+
+delegate void Method2<T1, T2>(T1 var1, T2 var2);
